8566b944ce24962fdc149844ffbc80163ce10e7e
Engkos <engkoskostaman97@gmail.com>
2019-08-13 08:49:01 +0700

    tugas 17 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas17.js/index.js
@@ -0,0 +1,9 @@
+function Ubah(){
+  var huruf ="Saya beLajar bahaSa peMrograman deNgan khUsuk"
+  var huruf1="Allen Sedang ikan di SuNgai,DIA MEndapat ikAN GaBus 3 Ekor dan LanGsuNg Dijual" 
+  console.log(huruf.toLowerCase());
+    console.log(huruf.toUpperCase());
+    console.log(huruf1.toLowerCase());
+      console.log(huruf1.toUpperCase());
+}
+Ubah();
